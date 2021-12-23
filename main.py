289b6bfe5e649258cdc04ea7b515b9bee018a424
Kylezzzz123 <95728517+Kylezzzz123@users.noreply.github.com>
2021-12-23 14:31:22 -0800

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((50,40))
-        self.image.fill(GREEN))
+        self.image.fill(GREEN)
         self.rect = self.image.get_rect()
         self.rect.x = 200
         self.rect.y = 200
